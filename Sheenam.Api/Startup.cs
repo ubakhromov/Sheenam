8b494f99e10c5320d0a62aa89b5528f63a93329d
tatubaqqol@gmail.com <tatubaqqol@gmail.com>
2022-08-28 21:59:26 +0500

    INRA: Create API Project

---
+++ Sheenam.Api/Startup.cs
@@ -0,0 +1,58 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use Comfort and Peace
+// ==================================================
+
+using Microsoft.OpenApi.Models;
+
+namespace Sheenam.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apiInfo = new OpenApiInfo();
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(
+                name: "v1",
+                info: apiInfo);
+            });
+
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                    url: "/swagger/v1/swagger.json",
+                    name: "Sheenam.Api v1");
+                    
+                });                   
+                    
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            endpoints.MapControllers());
+
+        }
+            
+    }
+}
