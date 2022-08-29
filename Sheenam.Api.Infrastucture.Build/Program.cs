3a7ef78b4053a001efac835339f7706e2cc2c279
tatubaqqol@gmail.com <tatubaqqol@gmail.com>
2022-08-29 15:01:38 +0500

    INFRA: Initialize Build Project

---
+++ Sheenam.Api.Infrastucture.Build/Program.cs
@@ -0,0 +1,73 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use Comfort and Peace
+// ==================================================
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Sheenam Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Check Out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Setting Up .Net",
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion ="6.0.300"
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring Nuget Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Building Project"
+                },
+
+                new TestTask
+                {
+                    Name ="Running Tests"
+                }
+
+            }
+        }
+    }
+};
+var client = new ADotNetClient();
+
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/dotnet.yml"); 
