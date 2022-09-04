d6d05fb55f8aab26143a37bd1ac2a8095b8eca2c
tatubaqqol@gmail.com <tatubaqqol@gmail.com>
2022-09-04 14:32:31 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> WIP

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Add.cs
@@ -0,0 +1,39 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync()
+        {
+            //given
+            Guest nullGuest = null;
+            var nullGuestException = new NullGuestException();
+
+            var expectedGuestValidationException =
+                new GuestValidationException(nullGuestException);
+
+            //when
+            ValueTask<Guest> addGuestTask =
+                this.guestServices.AddGuestAsync(nullGuest);
+
+            //then
+            await Assert.ThrowsAsync<GuestValidationException>(() =>
+            addGuestTask.AsTask());
+        }
+    }
+}
---
+++ Sheenam.Api/Models/Foundations/Guests/Exceptions/GuestValidationException.cs
@@ -0,0 +1,19 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Guests.Exceptions
+{
+    public class GuestValidationException : Xeption
+    {
+        public GuestValidationException(Xeption innerException)
+        : base(message: "Guest validation error occured, fix the errors and try again",
+              innerException)
+        { }
+
+    }
+}
