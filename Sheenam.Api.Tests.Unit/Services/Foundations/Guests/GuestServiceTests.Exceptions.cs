c8a605fd8b158654dc9e0541f81afea8ca7b75b1
tatubaqqol@gmail.com <tatubaqqol@gmail.com>
2022-09-16 02:02:53 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.cs
@@ -0,0 +1,53 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guest someGuest = CreateRandomGuest();
+            SqlException sqlException = GetSqlError();
+            var failedGuestStorageException = new FailedGuestStorageException(sqlException);
+
+            var expectedGuestDependencyException =
+                new GuestDependencyException(failedGuestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertGuestAsync(someGuest))
+                .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Guest> addGuestTask =
+                this.guestServices.AddGuestAsync(someGuest);
+
+            //then
+            await Assert.ThrowsAsync<GuestDependencyException>(() =>
+            addGuestTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertGuestAsync(someGuest),
+            Times.Once);
+
+            this.loggingBrokerMock.Verify(broker=>
+            broker.LogCrtitical(It.Is(SameExceptionAs(
+                expectedGuestDependencyException))),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
@@ -4,6 +4,8 @@
 // ==================================================
 
 using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
 using Moq;
 using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
@@ -39,6 +41,9 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
 
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 9).GetValue();
+
+        private static SqlException GetSqlError() =>
+            (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
 
         private static T GetInvalidEnum<T>()
         {
