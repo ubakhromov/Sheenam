8f1f0e9c5eb8e075abb0b5e6f464159ce52d7116
Ulugbek <ubakhromov22@gmail.com>
2022-10-04 17:22:16 +0500

    ShouldRetrieveGuestByIdAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,45 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveGuestByIdAsync()
+        {
+            //given
+            Guid randomGuestId = Guid.NewGuid();
+            Guid inputGuestId = randomGuestId;
+            Guest randomGuest = CreateRandomGuest();
+            Guest storageGuest = randomGuest;
+            Guest expectedGuest = storageGuest.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestsByIdAsync(inputGuestId))
+                    .ReturnsAsync(storageGuest);
+
+            //when
+            Guest actualGuest =
+                await this.guestServices.RetrieveGuestByIdAsync(inputGuestId);
+
+            //then
+            actualGuest.Should().BeEquivalentTo(expectedGuest);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestsByIdAsync(inputGuestId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
