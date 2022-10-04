aa92cf28b5bbe18d64dbf00a9073a1ee8ca240eb
Ulugbek <ubakhromov22@gmail.com>
2022-10-04 18:52:32 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestServices.Exceptions.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestServices.Exceptions.cs
@@ -31,6 +31,10 @@ namespace Sheenam.Api.Services.Foundations.Guests
             {
                 throw CreateAndLogValidationException(invalidGuestException);
             }
+            catch(NotFoundGuestException notFoundGuestException)
+            {
+                throw CreateAndLogValidationException(notFoundGuestException);
+            }
             catch (SqlException sqlException)
             {
                 var failedGuestStorageException =
