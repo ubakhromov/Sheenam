c481254ec32d0bb7485115b4001172d38d54d892
Ulugbek <ubakhromov22@gmail.com>
2022-10-11 18:39:25 +0500

    ShouldThrowValidationExceptionOnModifyIfGuestIsNullAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Modify.cs
@@ -0,0 +1,51 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfGuestIsNullAndLogItAsync()
+        {
+            //given
+            Guest nullGuest = null;
+            var nullGuestException = new NullGuestException();
+
+            var expectedGuestValidationException = 
+                new GuestValidationException(nullGuestException);
+
+            //when
+            ValueTask<Guest> modifyGuestTask =
+                this.guestServices.ModifyGuestAsync(nullGuest);
+
+            //then
+            await Assert.ThrowsAsync<GuestValidationException>(() =>
+                modifyGuestTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuestValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.SelectGuestsByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateGuestAsync(It.IsAny<Guest>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();            
+        }
+    }
+}
