c1e448fd8f18e57f167b7d72614d64be3f918b2a
Ulugbek <ubakhromov22@gmail.com>
2022-11-08 14:16:34 +0500

    CODE RUB: Fix Guest Model Migrations

---
+++ Sheenam.Api/Migrations/20221108090005_Guest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sheenam.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Guest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
