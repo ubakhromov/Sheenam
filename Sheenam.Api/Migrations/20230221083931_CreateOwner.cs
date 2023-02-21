423874263479c4e933c5c0768c93c21cc200e746
Ulugbek <ubakhromov22@gmail.com>
2023-02-21 13:47:11 +0500

    DATA: Add "CreatedDate" and "UpdatedDate"

---
+++ Sheenam.Api/Migrations/20230221083931_CreateOwner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sheenam.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOwner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "Owners",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "Owners",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Owners");
+        }
+    }
+}
--- Sheenam.Api/Models/Foundations/Owner/Owner.cs
+++ Sheenam.Api/Models/Foundations/Owner/Owner.cs
@@ -14,5 +14,7 @@ namespace Sheenam.Api.Models.Foundations.Owner
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public GenderType Gender { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
     }
 }
