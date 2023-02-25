7b0c438ec226eb1b04e676e877f8b976fb6b0869
Ulugbek <ubakhromov22@gmail.com>
2023-02-25 11:22:13 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Owners/OwnerServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,58 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Owner.Exceptions;
+using System;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Owners
+{
+    public partial class OwnerServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedOwnerStorageException(sqlException);
+
+            var expectedOwnerDependencyException =
+                new OwnerDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllOwners())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllOwnersAction = () =>
+                this.ownerService.RetrieveAllOwners();
+
+            OwnerDependencyException actualOwnerDependencyException =
+                Assert.Throws<OwnerDependencyException>(
+                    retrieveAllOwnersAction);
+
+            // then
+            actualOwnerDependencyException.Should().BeEquivalentTo(
+                expectedOwnerDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllOwners(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedOwnerDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
