f90ce417381c19f5f5a7702e6440f0682e5176bb
Ulugbek <ubakhromov22@gmail.com>
2023-02-26 11:29:28 +0500

    BROKERS: Update `Owner`

--- Sheenam.Api/Brokers/Storages/StorageBroker.Owners.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Owners.cs
@@ -24,5 +24,8 @@ namespace Sheenam.Api.Brokers.Storages
 
         public async ValueTask<Owner> SelectOwnerByIdAsync(Guid ownerId) =>
             await SelectAsync<Owner>(ownerId);
+
+        public async ValueTask<Owner> UpdateOwmerAsync(Owner owner) =>
+            await UpdateAsync(owner);
     }
 }
