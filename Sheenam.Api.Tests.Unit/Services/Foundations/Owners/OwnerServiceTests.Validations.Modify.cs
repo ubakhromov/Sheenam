4699e693fdf5a3b0400967ccf52ac148be5702dc
Ulugbek <ubakhromov22@gmail.com>
2023-02-26 13:30:16 +0500

    ShouldThrowValidationExceptionOnModifyIfOwnerIsInvalidAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Owners/OwnerServiceTests.Validations.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Owners/OwnerServiceTests.Validations.Modify.cs
@@ -55,5 +55,95 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Owners
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfOwnerIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidOwner = new Owner
+            {
+                FirstName = invalidText
+            };
+
+            var invalidOwnerException =
+                new InvalidOwnerException();
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.Id),
+                values: "Id is required");
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.FirstName),
+                values: "Text is required");
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.LastName),
+                values: "Text is required");
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.DateOfBirth),
+                values: "Date is required");
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.Email),
+                values: "Text is required");
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.CreatedDate),
+                values: "Date is required");
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.UpdatedDate),
+                values: "Date is required");
+
+            invalidOwnerException.AddData(
+                key: nameof(Owner.UpdatedDate),
+                    values: new[]
+                    {
+                        "Date is required",
+                        $"Date is the same as {nameof(Owner.CreatedDate)}",
+                        "Date is not recent"
+                    }
+                );
+
+            var expectedOwnerValidationException =
+                new OwnerValidationException(invalidOwnerException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(GetRandomDateTime);
+
+            // when
+            ValueTask<Owner> modifyOwnerTask =
+                this.ownerService.ModifyOwnerAsync(invalidOwner);
+
+            OwnerValidationException actualOwnerValidationException =
+                await Assert.ThrowsAsync<OwnerValidationException>(
+                    modifyOwnerTask.AsTask);
+
+            //then
+            actualOwnerValidationException.Should()
+                .BeEquivalentTo(expectedOwnerValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOwnerValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateOwnerAsync(It.IsAny<Owner>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
