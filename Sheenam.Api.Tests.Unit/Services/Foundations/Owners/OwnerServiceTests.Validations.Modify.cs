41539a803b42e81b6d43fdde2bb03555fb022b99
Ulugbek <ubakhromov22@gmail.com>
2023-02-26 13:11:30 +0500

    ShouldThrowValidationExceptionOnModifyIfOwnerIsNullAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Owners/OwnerServiceTests.Validations.Modify.cs
@@ -0,0 +1,59 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using Moq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+using Sheenam.Api.Models.Foundations.Owner;
+using Sheenam.Api.Models.Foundations.Owner.Exceptions;
+using FluentAssertions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Owners
+{
+    public partial class OwnerServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfOwnerIsNullAndLogItAsync()
+        {
+            // given
+            Owner nullOwner = null;
+            var nullOwnerException = new NullOwnerException();
+
+            var expectedOwnerValidationException =
+                new OwnerValidationException(nullOwnerException);
+
+            // when
+            ValueTask<Owner> modifyOwnerTask =
+                this.ownerService.ModifyOwnerAsync(nullOwner);
+
+            OwnerValidationException actualOwnerValidationException =
+                await Assert.ThrowsAsync<OwnerValidationException>(
+                modifyOwnerTask.AsTask);
+
+            // then
+            actualOwnerValidationException.Should()
+                .BeEquivalentTo(expectedOwnerValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedOwnerValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectOwnerByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateOwnerAsync(It.IsAny<Owner>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
