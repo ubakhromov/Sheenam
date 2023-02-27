630e6085bd5634f66ab1652553c339dbad1dd866
Ulugbek <ubakhromov22@gmail.com>
2023-02-27 16:34:39 +0500

    BROKERS: Delete `Owner`

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Owners.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Owners.cs
@@ -16,6 +16,6 @@ namespace Sheenam.Api.Brokers.Storages
         ValueTask<Owner> InsertOwnerAsync(Owner owner);
         IQueryable<Owner> SelectAllOwners();
         ValueTask<Owner> SelectOwnerByIdAsync(Guid ownerId);
-        ValueTask<Owner> UpdateOwnerAsync(Owner owner);
+        ValueTask<Owner> UpdateOwnerAsync(Owner owner);dsadd
     }
 }
