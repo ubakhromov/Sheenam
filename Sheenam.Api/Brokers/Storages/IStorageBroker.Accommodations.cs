2aea4f7a18c8e175595c8eed7b3d96d77278750d
Ulugbek <ubakhromov22@gmail.com>
2023-03-01 18:03:37 +0500

    CODE RUB: Add Interface

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Accommodations.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Accommodations.cs
@@ -1,6 +1,15 @@
-﻿namespace Sheenam.Api.Brokers.Storages
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Accommodations;
+
+namespace Sheenam.Api.Brokers.Storages
 {
-    public interface IStorageBroker
+    public partial interface IStorageBroker
     {
+        ValueTask<Accommodation> InsertAccommodationAsync(Accommodation accommodation);
     }
 }
