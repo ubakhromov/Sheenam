a231a1a7a9dbf2020a88d04e4554f935f3ab45b5
Ulugbek <ubakhromov22@gmail.com>
2023-03-01 22:10:54 +0500

    ShouldAddAccommodationAsync -> FAIL

---
+++ Sheenam.Api/Services/Foundations/Accommodations/AccommodationService.cs
@@ -0,0 +1,30 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using System;
+using System.Threading.Tasks;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Accommodations;
+
+namespace Sheenam.Api.Services.Foundations.Accommodations
+{
+    public class AccommodationService : IAccommodationService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public AccommodationService(
+            IStorageBroker storageBroker, 
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker; 
+        }
+
+        public ValueTask<Accommodation> AddAccommodationAsync(Accommodation accommodation) =>
+        throw new NotImplementedException();
+    }
+}
---
+++ Sheenam.Api/Services/Foundations/Accommodations/IAccommodationService.cs
@@ -0,0 +1,15 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Accommodations;
+
+namespace Sheenam.Api.Services.Foundations.Accommodations
+{
+    public interface IAccommodationService
+    {
+        ValueTask<Accommodation> AddAccommodationAsync(Accommodation accommodation);
+    }
+}
