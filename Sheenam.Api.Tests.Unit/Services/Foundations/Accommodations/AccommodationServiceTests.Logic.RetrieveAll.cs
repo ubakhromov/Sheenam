604552789d53f68a1dcd567358d3998ccd80660e
Ulugbek <ubakhromov22@gmail.com>
2023-03-07 10:49:28 +0500

    ShouldThrowServiceExceptionOnRetrieveAllWhenAllServiceErrorOccursAndLogIt -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Logic.RetrieveAll.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Logic.RetrieveAll.cs
@@ -6,6 +6,8 @@
 using FluentAssertions;
 using Moq;
 using Sheenam.Api.Models.Foundations.Accommodations;
+using Sheenam.Api.Models.Foundations.Accommodations.Exceptions;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -35,6 +37,48 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Accommodations
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectAllAccommodations(),
                     Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrieveAllWhenAllServiceErrorOccursAndLogIt()
+        {
+            // given
+            string exceptionMessage = GetRandomMessage();
+            var serviceException = new Exception(exceptionMessage);
+
+            var failedAccommodationServiceException =
+                new FailedAccommodationServiceException(serviceException);
+
+            var expectedAccommodationServiceException =
+                new AccommodationServiceException(failedAccommodationServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAccommodations())
+                    .Throws(serviceException);
+
+            // when
+            Action retrieveAllAccommodationsAction = () =>
+                this.accommodationService.RetrieveAllAccommodations();
+
+            AccommodationServiceException actualAccommodationServiceException =
+                Assert.Throws<AccommodationServiceException>(
+                    retrieveAllAccommodationsAction);
+
+            // then
+            actualAccommodationServiceException.Should().BeEquivalentTo(
+                expectedAccommodationServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAccommodations(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAccommodationServiceException))),
+                        Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
