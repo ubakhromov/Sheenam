d969d9482d9ebddaaf7c0f8abae8af3c33ee875f
Ulugbek <ubakhromov22@gmail.com>
2023-03-08 14:34:48 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,62 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Accommodations.Exceptions;
+using Sheenam.Api.Models.Foundations.Accommodations;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Accommodations
+{
+    public partial class AccommodationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedAccommodationStorageException =
+                new FailedAccommodationStorageException(sqlException);
+
+            var expectedAccommodationDependencyException =
+                new AccommodationDependencyException(failedAccommodationStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAccommodationByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Accommodation> retrieveAccommodationByIdTask =
+                this.accommodationService.RetrieveAccommodationByIdAsync(someId);
+
+            AccommodationDependencyException actaulAccommodationDependencyException =
+                await Assert.ThrowsAsync<AccommodationDependencyException>(
+                    retrieveAccommodationByIdTask.AsTask);
+
+            // then
+            actaulAccommodationDependencyException.Should().BeEquivalentTo(
+                expectedAccommodationDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAccommodationByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedAccommodationDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
