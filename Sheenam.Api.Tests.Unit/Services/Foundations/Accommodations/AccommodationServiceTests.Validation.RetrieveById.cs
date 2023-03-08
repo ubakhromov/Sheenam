d7a6d3548ddb382e7de8bcbda20246ca98efeccc
Ulugbek <ubakhromov22@gmail.com>
2023-03-08 12:31:11 +0500

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfAccommodationIsNotFoundAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Validation.RetrieveById.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Validation.RetrieveById.cs
@@ -56,5 +56,48 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Accommodations
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfAccommodationIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid someAccommodationId = Guid.NewGuid();
+            Accommodation noAccommodation = null;
+
+            var notFoundAccommodationException =
+                new NotFoundAccommodationException(someAccommodationId);
+
+            var expectedAccommodationValidationException =
+                new AccommodationValidationException(notFoundAccommodationException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAccommodationByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noAccommodation);
+
+            //when
+            ValueTask<Accommodation> retrieveAccommodationByIdTask =
+                this.accommodationService.RetrieveAccommodationByIdAsync(someAccommodationId);
+
+            AccommodationValidationException actualAccommodationValidationException =
+                await Assert.ThrowsAsync<AccommodationValidationException>(
+                    retrieveAccommodationByIdTask.AsTask);
+
+            // then
+            actualAccommodationValidationException
+                .Should().BeEquivalentTo(expectedAccommodationValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAccommodationByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAccommodationValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ Sheenam.Api/Models/Foundations/Accommodations/Exceptions/NotFoundAccommodationException.cs
@@ -0,0 +1,17 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using System;
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Accommodations.Exceptions
+{
+    public class NotFoundAccommodationException : Xeption
+    {
+        public NotFoundAccommodationException(Guid accommodationId)
+            : base(message: $"Couldn't find accommodation with id: {accommodationId}")
+        {}
+    }
+}
