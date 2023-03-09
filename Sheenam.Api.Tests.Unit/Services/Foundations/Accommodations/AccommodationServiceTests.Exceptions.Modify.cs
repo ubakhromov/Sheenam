a9e41745b7b8ebfe7eaf7c71a7606a5f1f4efdb7
Ulugbek <ubakhromov22@gmail.com>
2023-03-09 10:33:28 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,73 @@
+﻿//===================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+// ==================================================
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Accommodations.Exceptions;
+using Sheenam.Api.Models.Foundations.Accommodations;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Accommodations
+{
+    public partial class AccommodationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset someDateTime = GetRandomDateTimeOffset();
+            Accommodation randomAccommodation = CreateRandomAccommodation(someDateTime);
+            Accommodation someAccommodation = randomAccommodation;
+            Guid postId = someAccommodation.Id;
+            SqlException sqlException = GetSqlException();
+
+            var failedAccommodationStorageException =
+                new FailedAccommodationStorageException(sqlException);
+
+            var expectedAccommodationDependencyException =
+                new AccommodationDependencyException(failedAccommodationStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<Accommodation> modifyAccommodationTask =
+                this.accommodationService.ModifyAccommodationAsync(someAccommodation);
+
+            AccommodationDependencyException actualAccommodationDependencyException =
+              await Assert.ThrowsAsync<AccommodationDependencyException>(
+                  modifyAccommodationTask.AsTask);
+
+            // then
+            actualAccommodationDependencyException.Should().BeEquivalentTo(
+                expectedAccommodationDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAccommodationByIdAsync(postId),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedAccommodationDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateAccommodationAsync(someAccommodation),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
