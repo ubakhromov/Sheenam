c2b45ca3737c946164436153eebe3f23f1914c3d
Ulugbek <ubakhromov22@gmail.com>
2023-03-09 09:50:19 +0500

    ShouldThrowValidationExceptionOnModifyIfAccommodationIsInvalidAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Validation.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Validation.Modify.cs
@@ -54,5 +54,95 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Accommodations
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfAccommodationIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidAccommodation = new Accommodation
+            {
+                Address = invalidText
+            };
+
+            var invalidAccommodationException =
+                new InvalidAccommodationException();
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.Id),
+                values: "Id is required");
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.OwnerId),
+                values: "Id is required");
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.Address),
+                values: "Text is required");
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.Area),
+                values: "Area is required");
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.Price),
+                values: "Price is required");
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.CreatedDate),
+                values: "Date is required");
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.UpdatedDate),
+                values: "Date is required");
+
+            invalidAccommodationException.AddData(
+                key: nameof(Accommodation.UpdatedDate),
+                    values: new[]
+                    {
+                        "Date is required",
+                        $"Date is the same as {nameof(Accommodation.CreatedDate)}",
+                        "Date is not recent"
+                    }
+                );
+
+            var expectedAccommodationValidationException =
+                new AccommodationValidationException(invalidAccommodationException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(GetRandomDateTimeOffset);
+
+            // when
+            ValueTask<Accommodation> modifyAccommodationTask =
+                this.accommodationService.ModifyAccommodationAsync(invalidAccommodation);
+
+            AccommodationValidationException actualAccommodationValidationException =
+                await Assert.ThrowsAsync<AccommodationValidationException>(
+                    modifyAccommodationTask.AsTask);
+
+            //then
+            actualAccommodationValidationException.Should()
+                .BeEquivalentTo(expectedAccommodationValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedAccommodationValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateAccommodationAsync(It.IsAny<Accommodation>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
