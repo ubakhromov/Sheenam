dd5f805120b0db26e792b975e38b55fff4f58989
Ulugbek <ubakhromov22@gmail.com>
2023-03-09 12:46:43 +0500

    ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync -> PASS

--- Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Exceptions.RemoveById.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Accommodations/AccommodationServiceTests.Exceptions.RemoveById.cs
@@ -6,10 +6,13 @@
 using System;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Sheenam.Api.Models.Foundations.Accommodations;
 using Sheenam.Api.Models.Foundations.Accommodations.Exceptions;
+using Sheenam.Api.Models.Foundations.Accommodations.Exceptions;
+using Sheenam.Api.Models.Foundations.Accommodations;
 using Xunit;
 
 namespace Sheenam.Api.Tests.Unit.Services.Foundations.Accommodations
@@ -59,6 +62,48 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Accommodations
             this.storageBrokerMock.Verify(broker =>
                 broker.DeleteAccommodationAsync(It.IsAny<Accommodation>()),
                     Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someAccommodationId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedAccommodationStorageException =
+                new FailedAccommodationStorageException(sqlException);
+
+            var expectedAccommodationDependencyException =
+                new AccommodationDependencyException(failedAccommodationStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAccommodationByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+            // when
+            ValueTask<Accommodation> deleteAccommodationTask =
+                this.accommodationService.RemoveAccommodationByIdAsync(someAccommodationId);
+
+            AccommodationDependencyException actualAccommodationDependencyException =
+                await Assert.ThrowsAsync<AccommodationDependencyException>(
+                    deleteAccommodationTask.AsTask);
+
+            // then
+            actualAccommodationDependencyException.Should().BeEquivalentTo(
+                expectedAccommodationDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAccommodationByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedAccommodationDependencyException))),
+                        Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
